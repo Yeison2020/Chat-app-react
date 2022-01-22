2afed2f61e2c40103aa394815859eaac430530e8
Yeison <enmanuelquiterios@gmail.com>
2022-01-22 13:44:27 -0500

    Added Flow for Messages

--- src/component/ChatFeed.js
+++ src/component/ChatFeed.js
@@ -4,8 +4,64 @@ import MyMessage from "./MyMessage";
 import TheirMessage from "./TheirMessage";
 
 const ChatFeed = (props) => {
-  console.log(props);
-  return <div>ChatFeed</div>;
+  const { chats, activeChat, userName, messages } = props;
+  const chat = chats && chats[activeChat];
+  console.log(chat, userName, messages);
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+    return keys.map((key, index) => {
+      // Meesage Key value
+      const message = messages[key];
+
+      // Finding last message
+      const lastMessageKey = index === 0 ? null : key[index - 1];
+
+      // Checking If message belongs to me
+      const isMyMessage = userName === message.sender.username;
+      return (
+        <div key={`msg_${index}`} style={{ width: `100%` }}>
+          <div className={`message-block`}>
+            {isMyMessage ? (
+              <MyMessage message={message} />
+            ) : (
+              <TheirMessage
+                message={message}
+                lastMessage={messages[lastMessageKey]}
+              />
+            )}
+          </div>
+          <div
+            className={`read-receipts`}
+            style={{
+              marginRight: isMyMessage ? "18px" : "0px",
+              marginLeft: isMyMessage ? "0px" : "68px",
+            }}
+          >
+            read-receipts
+          </div>
+        </div>
+      );
+    });
+  };
+
+  if (!chat) return "Loading...";
+
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => `${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: "100px" }} />
+      <div className="messages-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
+  );
 };
 
 export default ChatFeed;
