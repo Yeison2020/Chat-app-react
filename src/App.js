e0f07127185906eb7385c4d6f8ed0be44c73db60
Yeison <enmanuelquiterios@gmail.com>
2022-01-22 13:09:59 -0500

    Created Chat home page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="
+  d09a9612-214f-49da-aaf9-4357866ea070"
+      userName="yeison695"
+      userSecret="123123"
+    />
+  );
+};
+
+export default App;
