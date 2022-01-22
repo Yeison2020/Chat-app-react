71fd890367e4db107e7bbf7ca8205e1a832afb70
Yeison <enmanuelquiterios@gmail.com>
2022-01-22 13:18:17 -0500

    Adding components under my chatFeed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
+import ChatFeed from "./component/ChatFeed";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
   d09a9612-214f-49da-aaf9-4357866ea070"
       userName="yeison695"
       userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
---
+++ src/component/MyMessage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyMessage = () => {
+  return <div>My Message</div>;
+};
+
+export default MyMessage;
